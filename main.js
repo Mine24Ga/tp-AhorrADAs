15d92643a03ae0c688f117df3afcdc6a305811e3
Mine24Ga <Minerva.Guanipa1998@gmail.com>
2023-01-11 19:34:24 -0300

    agregando los id perdidos con el merge

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-//Menú hamburguesa
+//Botones del header
+const btnBalance = document.getElementById('btn-balance');
+const btnCategorias = document.getElementById('btn-categories');
+const btnReportes = document.getElementById('btn-reports');
 const menuBurger = document.getElementById("navbar-burger");
+
+//Menú hamburguesa
+
 const menuNavbar = document.getElementById("navbar-menu");
 menuBurger.addEventListener("click", () => {
     menuBurger.classList.toggle("is-active");
