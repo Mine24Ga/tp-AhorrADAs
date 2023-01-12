f9b5ef07debd71df948bfd454db8a08c45c5e596
Mine24Ga <Minerva.Guanipa1998@gmail.com>
2023-01-12 18:55:53 -0300

    funcionalidad a las vistas y arreglo de html

--- main.js
+++ main.js
@@ -1,120 +1,151 @@
 /*--------------------------------- BOTONES DEL HEADER ------------------------------------*/
-const btnBalance = document.getElementById('btn-balance');
-const btnCategorias = document.getElementById('btn-categories');
-const btnReportes = document.getElementById('btn-reports');
+const btnBalance = document.getElementById("btn-balance");
+const btnCategories = document.getElementById("btn-categories");
+const btnReports = document.getElementById("btn-reports");
 const menuBurger = document.getElementById("navbar-burger");
 
 /*---------------------------------- SECCIONES ------------------------------------------*/
-const seccionBalance = document.getElementById('section-balance');
-const seccionNuevaOperacion = document.getElementById('section-new-operation');
-const seccionEditarOperacion = document.getElementById('section-edit-operation');
-const seccionCategorias = document.getElementById('section-category');
-const seccionEditarCategoria = document.getElementById('section-edit-category');
-const seccionReportes = document.getElementById('section-reports');
+//Contenedores
+const sectionBalance = document.getElementById("section-balance");
+const sectionNewOperation = document.getElementById("section-new-operation");
+const sectionEditOperation = document.getElementById(
+  "section-edit-operation"
+);
+const sectionCategory = document.getElementById("section-category");
+const sectionReports = document.getElementById("section-reports");
+const sectionEditCategory = document.getElementById("section-edit-category");
 const menuNavbar = document.getElementById("navbar-menu");
 
 /*---------------------------------------- BALANCE -----------------------------------------*/
 //Sección Balance
-const balanceGanancias = document.getElementById("balance-gain");
-const balanceGastos = document.getElementById("balance-expense");
+const balanceEarnings = document.getElementById("balance-gain");
+const balanceExpense = document.getElementById("balance-expense");
 const balanceTotal = document.getElementById("balance-total");
-
 
 /*--------------------------------------- FILTROS --------------------------------------------*/
 //Botón para ocultar y mostrar filtros
-const btnOcultarFiltros = document.getElementById('btn-hidden-filter');
-const filtros = document.getElementById('filters');
-//filtros para selecionar 
-const filtroTipo = document.getElementById('filter-type');
-const selectCategorias = document.getElementById('filter-category');
-const filtroFecha = document.getElementById('filter-date');
-const filtroOrdenar = document.getElementById('filter-organize');
+const btnHideFilters = document.getElementById("btn-hidden-filters");
+const filters = document.getElementById("filters");
+
+//filtros para selecionar
+const filterType = document.getElementById("filter-type");
+const selectFilterCategories = document.getElementById("filter-category");
+const filterDate = document.getElementById("filter-date");
+const filterSort = document.getElementById("filter-organize");
 
 /*--------------------------------------- OPERACIONES -------------------------------------- */
 //Botón nueva operación
-const btnNuevaOperacion = document.getElementById('btn-new-operation');
-//Tabla de operaciones / sin operaciones
-const tablaOperaciones = document.getElementById("operations-table");
-const sinOperaciones = document.getElementById("no-operation");
-//mostrar en balance 
-const mostrarEnBalance = document.getElementById('create-operation');
-//Inputs nueva operación
-const inputDescripcion = document.getElementById('input-description');
-const inputMonto = document.getElementById('input-amount');
-const inputTipo = document.getElementById('input-type');
+const btnNewOperation = document.getElementById("btn-new-operation");
 
-const inputFecha = document.getElementById('input-date');
+//Tabla de operaciones / sin operaciones
+const operationsTable = document.getElementById("operations-table");
+const noOperations = document.getElementById("no-operation");
+
+//mostrar en balance
+const showOnStock = document.getElementById("create-operacion");
+
+//Inputs nueva operación
+const inputDescription = document.getElementById("input-description");
+const inputAmount = document.getElementById("input-amount");
+const inputType = document.getElementById("input-type");
+const inputDate = document.getElementById("input-date");
+
 //Botón para cancelar o agregar en nueva operacion
-const btnCancelar = document.getElementById('btn-cancel');
-const btnAgregar = document.getElementById('btn-add');
+const btnCancel = document.getElementById("btn-cancel");
+const btnAdd = document.getElementById("btn-add");
 
 /*------------------------------------- EDITAR OPERACIONES -----------------------------------*/
 //Inputs editar operación
-const inputEditarDescripcion = document.getElementById('input-description-edit');
-const inputEditarMonto = document.getElementById('input-amount-edit');
-const inputEditarTipo = document.getElementById('input-type-edit');
-const selectEditarCategoria = document.getElementById('category-select-edit');
-const inputEditarFecha = document.getElementById('input-date-edit');
+const inputEditDescription = document.getElementById(
+  "input-description-edit"
+);
+const inputEditAmount = document.getElementById("input-amount-edit");
+const inputEditType = document.getElementById("input-type-edit");
+const selectEditCategory = document.getElementById("category-select-edit");
+const inputEditDate = document.getElementById("input-date-edit");
+
 //Botones para cancelar o editar en editar operacion
-const btnCancelarEditar = document.getElementById('btn-cancel-edit');
-const btnEditarOperacion = document.getElementById('btn-edit');
+const btnCancelEdit = document.getElementById("btn-cancel-edit");
+const btnEditEdit = document.getElementById("btn-edit");
 
-/*---------------------------------------- CATEGORIA --------------------------------------- */
-const inputCategoria = document.getElementById('input-category');
-const btnAgregarCategoria = document.getElementById('btn-add-category');
-const listaCategorias = document.getElementById('categories-list');
+/*---------------------------------------- CATEGORIA ---------------------------------------*/
+//categorias
+const inputCategory = document.getElementById("input-category");
+const btnAddCategory = document.getElementById("btn-add-category");
+const categoryList = document.getElementById("categories-list");
+
 //Editar categorias
-const inputEditarCategoria = document.getElementById('input-edit-category');
-const btnCancelarCategoria = document.getElementById('btn-cancel-edit-category');
-const btnEditarCategoria = document.getElementById('btn-edit-category');
-const selectCategoriasOperacion = document.getElementById('category-select');
+const inputEditCategory = document.getElementById("input-edit-category");
+const btnCancelEditCategory = document.getElementById(
+  "btn-cancel-edit-category"
+);
+const btnEditCategory = document.getElementById("btn-edit-category");
+const selectCategories = document.getElementById("category-select");
 
-/*---------------------------------------------- REPORTES --------------------------------- */
-const sinReportes = document.getElementById("no-reports");
-const conReportes = document.getElementById("with-reports");
-const reporteResumen = document.getElementById("report-summary");
-const reporteTotalCategoria = document.getElementById("total-category-reports");
-const reporteTotalMes = document.getElementById("total-month-reports");
+/*---------------------------------------------- REPORTES --------------------------------- 
+const noReports = document.getElementById("no-reports");
+const withReports = document.getElementById("with-reports");
+const summaryReport = document.getElementById("report-summary");
+const totalCategoryReport = document.getElementById("total-category-reports");
+const totalMonthReport= document.getElementById("total-month-reports");
 
 
 _______________________________________________________________________________________________
-/*----------------------------------------- EVENTOS ----------------------------------------- */
+/*----------------------------------------- EVENTOS -----------------------------------------*/
 //Menú hamburguesa
 menuBurger.addEventListener("click", () => {
-    menuBurger.classList.toggle("is-active");
-    menuNavbar.classList.toggle("is-active");
+  menuBurger.classList.toggle("is-active");
+  menuNavbar.classList.toggle("is-active");
 });
 
-
+//Vistas
 //Boton balance
-btnBalance.addEventListener('click', () => {
-    seccionBalance.style.display = 'block'
-    seccionCategorias.style.display = 'none'
-    seccionReportes.style.display = 'none'
-    seccionNuevaOperacion.style.display = 'none'
-    seccionEditarOperacion.style.display = 'none'
-    seccionEditarCategoria.style.display = 'none'
-  })
-  
-  //Botón categorias
-  btnCategorias.addEventListener('click', () => {
-    seccionBalance.style.display = 'none'
-    seccionCategorias.style.display = 'block'
-    seccionReportes.style.display = 'none'
-    seccionNuevaOperacion.style.display = 'none'
-    seccionEditarOperacion.style.display = 'none'
-    seccionEditarCategoria.style.display = 'none'
-  })
-  
-  //Botón reportes
-  btnReportes.addEventListener('click', () => {
-    seccionBalance.style.display = 'none'
-    seccionCategorias.style.display = 'none'
-    seccionReportes.style.display = 'block'
-    seccionNuevaOperacion.style.display = 'none'
-    seccionEditarOperacion.style.display = 'none'
-    seccionEditarCategoria.style.display = 'none'
-   
-  })
+btnBalance.addEventListener("click", () => {
+  sectionBalance.style.display = "block";
+  sectionCategory.style.display = "none";
+  sectionReports.style.display = "none";
+  sectionNewOperation.style.display = "none";
+  sectionEditOperation.style.display = "none";
+  sectionEditCategory.style.display = "none";
+});
 
+//Botón categorias
+btnCategories.addEventListener("click", () => {
+  sectionBalance.style.display = "none";
+  sectionCategory.style.display = "block";
+  sectionReports.style.display = "none";
+  sectionNewOperation.style.display = "none";
+  sectionEditOperation.style.display = "none";
+  sectionEditCategory.style.display = "none";
+});
 
+//Botón reportes
+btnReports.addEventListener("click", () => {
+  sectionBalance.style.display = "none";
+  sectionCategory.style.display = "none";
+  sectionReports.style.display = "block";
+  sectionNewOperation.style.display = "none";
+  sectionEditOperation.style.display = "none";
+  sectionEditCategory.style.display = "none";
+  
+});
+
+//Botón nueva operación
+btnNewOperation.addEventListener("click", () => {
+  sectionBalance.style.display = "none";
+  sectionNewOperation.style.display = "block";
+});
+//Botón cancelar
+btnCancel.addEventListener("click", () => {
+  sectionBalance.style.display = "block";
+  sectionNewOperation.style.display = "none";
+});
+//Botón editar-cancelar
+btnCancelEdit.addEventListener("click", () => {
+  sectionBalance.style.display = "block";
+  sectionEditOperation.style.display = "none";
+});
+
+/*Botones para editar o eliminar en página balance
+const btnEliminarOperacion = document.getElementById('btn-eliminar-operacion');
+const btnEditarOperacion = document.getElementById('btn-editar-operation');*/
