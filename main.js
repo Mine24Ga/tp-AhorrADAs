99d144961ea50c0e27ce53b2ce8ca9139375914c
Mine24Ga <Minerva.Guanipa1998@gmail.com>
2023-01-12 22:01:36 -0300

    generar una operacion y mostrarla en balance

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ const operationsTable = document.getElementById("operations-table");
 const noOperations = document.getElementById("no-operation");
 
 //mostrar en balance
-const showOnStock = document.getElementById("create-operacion");
+const showOnStock = document.getElementById("create-operation");
 
 //Inputs nueva operación
 const inputDescription = document.getElementById("input-description");
@@ -146,9 +146,9 @@ btnCancelEdit.addEventListener("click", () => {
   sectionEditOperation.style.display = "none";
 });
 
-/*Botones para editar o eliminar en página balance
+//Botones para editar o eliminar en página balance
 const btnEliminarOperacion = document.getElementById('btn-eliminar-operacion');
-const btnEditarOperacion = document.getElementById('btn-editar-operation');*/
+const btnEditarOperacion = document.getElementById('btn-editar-operacion');
 
 //Botón para ocultar y mostrar los filtros
 btnHideFilters.addEventListener('click', () => {
@@ -160,3 +160,127 @@ btnHideFilters.addEventListener('click', () => {
     filters.style.display = 'block'
   }
 });
+
+
+/*-------------------------------------- OPERACIONES ----------------------------------------*/
+//Input fecha 
+const date = () => {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  return `${year}-${month < 10 ? "0" + month : month}-${
+    day < 10 ? "0" + day : day
+  }`;
+};
+
+inputDate.value = date();
+inputEditDate.value = inputDate.value;
+filterDate.value = inputDate.value;
+
+//generar id
+const generateId = () => {
+  let p1 = Math.floor(Math.random() * 0x10000);
+  let p2 = new Date().getTime();
+  return `${p1}${p2}`;
+};
+
+
+//Formulario para agregar nueva operacion
+const resetForm = () => {
+  inputDescription.value = "";
+  inputAmount.value = 0;
+  inputType.value = "gasto";
+  selectCategories.value = "";
+  inputDate.value = date();
+};
+
+//Ocultar y mostrar imagen/tabla
+const reviewOperations = (operations) => {
+  //Cuando no haya operaciones, ocultar tabla y mostrar imagen.
+if (operations == 0){
+  operationsTable.style.display = 'none' ;
+  noOperations.style.display = 'block' ;
+  //Y cuando haya operaciones, ocultar imagen y mostrar tabla  
+} else {
+  operationsTable.style.display = 'block' ;
+  noOperations.style.display = 'none' ;
+}
+};
+
+let operations = [];
+let balanceView = [];
+
+
+//Botón para agregar operación (despuès de rellenar el formulario)
+btnAdd.addEventListener('click', () => {
+
+  const showOperation = {
+    id: generateId(),
+    descripcion: inputDescription.value,
+    monto: inputAmount.value,
+    tipo: inputType.value,
+    categoria: selectCategories.value,
+    fecha: inputDate.value,
+  }
+
+  if (showOperation.tipo === "gasto") {
+    showOperation.monto = Number(showOperation.monto) * -1;
+  }
+
+  operations.push(showOperation);
+  localStorage.setItem('storageOperations', JSON.stringify(operations));
+  const takeOperations = JSON.parse(localStorage.getItem('storageOperations'));
+
+  resetForm();
+  addBalanceOperation(takeOperations);
+  balanceHTML(takeOperations);
+
+  //Volver a la vista de Balance
+  sectionBalance.style.display = 'block'
+  sectionNewOperation.style.display = 'none' 
+});
+
+
+//Mostrar operacion en balance      
+const addBalanceOperation = (operations) => {
+  showOnStock.innerHTML = '';
+  reviewOperations(operations)
+  for (let i = 0; i < operations.length; i++) {
+    
+    const box =
+
+    `
+    <div id="${operations[i].id}">
+      <div class="mb-3">
+        <div class="columns is-multiline is-mobile is-vcentered">
+          <div class="column is-3-tablet is-6-mobile">
+            <h3 class="has-text-weight-semibold is-size-6">${operations[i].descripcion}</h3>
+          </div>
+
+          <div class="column is-3-tablet is-6-mobile has-text-right-mobile">
+            <span class="tag is-info is-light is-rounded is-size-6">${operations[i].categoria}</span>
+          </div>
+
+          <div class="column is-2-tablet has-text-grey is-hidden-mobile has-text-left-tablet is-size-6">
+          ${operations[i].fecha}
+          </div>
+          
+          <div class="column is-2 has-text-right is-size-6 ${operations[i].tipo === 'ganancia' ? 'tag is-primary is-light is-rounded' : 'tag is-danger is-light is-rounded'}"> $${operations[i].monto}
+          </div>
+
+          <div class="column is-2-tablet has-text-right">
+            <button class="button is-inverted tag is-link is-size-6" onclick="editarOperacion('${operations[i].id}')"><i class="fas fa-pen"></i></i></button>
+            <button class="button is-inverted tag is-danger is-size-6" onclick="eliminarOperacion('${operations[i].id}')"><i class="fas fa-trash-alt"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>`
+
+    showOnStock.insertAdjacentHTML('beforeend', box)
+  }
+}
+
+operations = JSON.parse(localStorage.getItem("storageOperations")) ?? operations;
+addBalanceOperation(operations);
+reviewOperations(operations)
